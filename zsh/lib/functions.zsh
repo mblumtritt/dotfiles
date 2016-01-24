993d0a351107995fa0e7a14108f7c50b7900de00
Mike Blumtritt <mike.blumtritt@injixo.com>
2016-01-24 19:16:07 +0100

    Correct function `gp`
    Need to remove the \r\n chars at the end!

--- zsh/lib/functions.zsh
+++ zsh/lib/functions.zsh
@@ -2,4 +2,4 @@ lsr () { ls **/*$1* } # ls subdirs
 h () { history 0 | grep $1 } # find in history
 take () { mkdir -p $1; cd $1 } # create dir and step into
 theme () { source ~/.zsh/themes/$1.zsh-theme } # select a theme
-gp () { security find-generic-password -l $1 -w | pbcopy } # store password from key chain in clipboard
+gp () { security find-generic-password -l $1 -w | tr -d '\r\n' | pbcopy } # store password from key chain in clipboard
