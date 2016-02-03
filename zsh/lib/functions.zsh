0f8eee3a083aa33ac65551aed83b32b14ee05b0b
Mike Blumtritt <mike.blumtritt@injixo.com>
2016-02-03 09:58:41 +0100

    Tidy up functions

--- zsh/lib/functions.zsh
+++ zsh/lib/functions.zsh
@@ -1,5 +1,7 @@
-lsr () { ls **/*$1* } # ls subdirs
-h () { history 0 | grep $1 } # find in history
-take () { mkdir -p $1; cd $1 } # create dir and step into
-theme () { source ~/.zsh/themes/$1.zsh-theme } # select a theme
-gp () { security find-generic-password -l $1 -w | tr -d '\r\n' | pbcopy } # store password from key chain in clipboard
+function lsr () { ls **/*$1* } # ls subdirs
+function h () { history 0 | grep $1 } # find in history
+function mkcd() { mkdir -p "$@" && cd "$_"; } # create dir and step into
+function f() { find . -iname "*$1*" ${@:2} } # find file
+function r() { grep "$1" ${@:2} -R . } # find file containing
+function theme () { source ~/.zsh/themes/$1.zsh-theme } # select a theme
+function gp () { security find-generic-password -l $1 -w | tr -d '\r\n' | pbcopy } # store password from key chain in clipboard
