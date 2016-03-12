e15e4ae20440f48f8fbf794703b108d366ddda85
Mike Blumtritt <mike.blumtritt@injixo.com>
2016-03-12 21:48:58 +0100

    Use PA and remove unused things

--- zsh/lib/functions.zsh
+++ zsh/lib/functions.zsh
@@ -4,4 +4,4 @@ function mkcd() { mkdir -p "$@" && cd "$_"; } # create dir and step into
 function f() { find . -iname "*$1*" ${@:2} } # find file
 function r() { grep "$1" ${@:2} -R . } # find file containing
 function theme () { source ~/.zsh/themes/$1.zsh-theme } # select a theme
-function gp () { security find-generic-password -l $1 -w | tr -d '\r\n' | pbcopy } # store password from key chain in clipboard
+function cdp() { cd $(i prj find --top "$@") }
