62093a4799edc829ce5b307fbbf7577753044279
Mike Blumtritt <mike.blumtritt@injixo.com>
2016-04-15 14:04:59 +0200

    correct `cdp` function

--- zsh/lib/functions.zsh
+++ zsh/lib/functions.zsh
@@ -3,4 +3,4 @@ function h () { history 0 | grep $1 } # find in history
 function mkcd() { mkdir -p "$@" && cd "$_"; } # create dir and step into
 function f() { find . -iname "*$1*" ${@:2} } # find file
 function r() { grep "$1" ${@:2} -R . } # find file containing
-function cdp() { cd $(i prj find --top "$@") }
+function cdp() { cd $(i prj --top "$@") }
