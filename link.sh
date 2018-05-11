c36147a52fff6200fb1da7b96e61ea85dd033e68
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-05-11 11:29:48 +0200

    Link binaries to user binaries dir

--- link.sh
+++ link.sh
@@ -1,25 +1,41 @@
 #! /usr/bin/env bash
 
-readonly SOURCE_DIR="$HOME/.usr/dotfiles/files"
+readonly DOT_SOURCE_DIR="$HOME/.usr/dotfiles/files"
+readonly BIN_SOURCE_DIR="$HOME/.usr/dotfiles/bin"
 readonly BACKUP_DIR="$HOME/_backup/$(date +%s)"
 
 function backup() {
-  echo "backup $1 to $BACKUP_DIR"
-  mkdir -p "$BACKUP_DIR"
-  mv "$1" "$BACKUP_DIR"
+	echo "backup $1 to $BACKUP_DIR"
+	mkdir -p "$BACKUP_DIR"
+	mv "$1" "$BACKUP_DIR"
 }
 
 function is-dotfile() {
-  basename "$1" | grep -v '^.DS_Store$\|^_darwin$\|^_linux$' > /dev/null 2>&1
+	basename "$1" | grep -v '^.DS_Store$\|^_darwin$\|^_linux$' > /dev/null 2>&1
 }
 
-for src_file in "$SOURCE_DIR"/* ; do
-  is-dotfile "$src_file" || continue
-  dst_filename=".$(basename "$src_file")"
-  dst_file="$HOME/$dst_filename"
-  if [ "$(readlink "$dst_file")" != "$src_file" ] ; then
-    test -e "$dst_file" && backup "$dst_file"
-    echo "create link $dst_filename"
-    ln -sf "$src_file" "$dst_file"
-  fi
+for src_file in "$DOT_SOURCE_DIR"/*
+do
+	is-dotfile "$src_file" || continue
+	dst_filename=".$(basename "$src_file")"
+	dst_file="$HOME/$dst_filename"
+	if [ "$(readlink "$dst_file")" != "$src_file" ]
+	then
+		test -e "$dst_file" && backup "$dst_file"
+		echo "create link $dst_filename"
+		ln -sf "$src_file" "$dst_file"
+	fi
+done
+
+for src_file in "$BIN_SOURCE_DIR"/*
+do
+	is-dotfile "$src_file" || continue
+	dst_filename="$(basename "$src_file")"
+	dst_file="$HOME/.usr/bin/$dst_filename"
+	if [ "$(readlink "$dst_file")" != "$src_file" ]
+	then
+		test -e "$dst_file" && backup "$dst_file"
+		echo "create link $dst_filename"
+		ln -sf "$src_file" "$dst_file"
+	fi
 done
