f715d27e29c867f02dc29fff722458189c20fae2
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-05-11 11:06:03 +0200

    Optimize zshrc
    setup interactive things only for interactive shells

--- files/zsh/lib/functions.zsh
+++ files/zsh/lib/functions.zsh
@@ -3,3 +3,4 @@ function cdp() { cd $(i prj --top "$@") }
 function mkcd() { mkdir -p "$@" && cd "$_"; } # create dir and step into
 function f() { find . -iname "*$1*" ${@:2} } # find file
 function r() { grep "$1" ${@:2} -R . } # find file containing
+function source_if_exists() { [[ -f "$1" ]] && source "$1"; } # source only if file exists
