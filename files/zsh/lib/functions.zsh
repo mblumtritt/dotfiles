52ccc76bf75cb0caae2ffd9086af74b462fff3e2
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-05-12 02:02:05 +0200

    Remove function for project path guessing

--- files/zsh/lib/functions.zsh
+++ files/zsh/lib/functions.zsh
@@ -1,5 +1,5 @@
 function h () { history 0 | grep $1 } # find in history
-function cdp() { cd $(i prj --top "$@") }
+# function cdp() { cd $(i prj --top "$@") }
 function mkcd() { mkdir -p "$@" && cd "$_"; } # create dir and step into
 function f() { find . -iname "*$1*" ${@:2} } # find file
 function r() { grep "$1" ${@:2} -R . } # find file containing
