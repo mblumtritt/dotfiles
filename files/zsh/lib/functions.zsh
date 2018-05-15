56faf0658fdb0308aeb37a5317eb66a4da706f65
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-05-15 18:03:02 +0200

    Fix tr() function

--- files/zsh/lib/functions.zsh
+++ files/zsh/lib/functions.zsh
@@ -4,7 +4,7 @@ function f() { find . -iname "*$1*" ${@:2} } # find file
 function r() { grep "$1" ${@:2} -R . } # find file containing
 # function cdp() { cd $(i prj --top "$@") }
 function source_if_exists() { [[ -f "$1" ]] && source "$1"; } # source only if file exists
-function tr() { web-view "$(web-find --dict "$1")" | convert-html-text | less } # lookup in GE/EN dictionary
+function tr() { web-view "$(web-find --dict "$1")" | convert-html-txt | less } # lookup in GE/EN dictionary
 function google() { open "$(web-find --google "$1")" } # Google search
 function img() { open "$(web-find --image "$1")" } # Google image search
 function vid() { open "$(web-find --video "$1")" } # YouTube image search
