53273002e2f56a8d2bc476679ab954462fda555f
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-05-16 08:39:53 +0200

    (Re-)implement cdp() function using prj-find

--- files/zsh/lib/functions.zsh
+++ files/zsh/lib/functions.zsh
@@ -2,7 +2,7 @@ function h () { history 0 | grep $1 } # find in history
 function mkcd() { mkdir -p "$@" && cd "$_"; } # create dir and step into
 function f() { find . -iname "*$1*" ${@:2} } # find file
 function r() { grep "$1" ${@:2} -R . } # find file containing
-# function cdp() { cd $(i prj --top "$@") }
+function cdp() { cd $(prj-find --top "$@") }
 function source_if_exists() { [[ -f "$1" ]] && source "$1"; } # source only if file exists
 function tr() { web-view "$(web-find --dict "$1")" | convert-html-txt | less } # lookup in GE/EN dictionary
 function google() { open "$(web-find --google "$1")" } # Google search
