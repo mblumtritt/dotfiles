a7923cfd37368710c86f16815d8ff460b3ba270a
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2018-05-17 21:11:37 +0200

    Extend init-tacos.sh

--- init-macos.sh
+++ init-macos.sh
@@ -8,6 +8,12 @@ then
 	chsh -s "$(which zsh)"
 fi
 
+echo "General: xpand save panel by default"
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+
+echo "General: expand print panel by default"
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+
 echo "General: enable TAB to select all controls in dialogs"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
@@ -23,14 +29,27 @@ defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 echo "General: save screenshots in PNG format"
 defaults write com.apple.screencapture type -string "png"
 
+echo "General: disable shadow in screenshots"
+defaults write com.apple.screencapture disable-shadow -bool true
+
 echo "General: don’t show Dashboard as a Space"
 defaults write com.apple.dock dashboard-in-overlay -bool true
+
+echo "General: request password asap"
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
 
 echo "Finder: automatically hide and show the Dock"
 defaults write com.apple.dock autohide -bool true
 
+echo "Finder: show indicator lights for open applications"
+defaults write com.apple.dock show-process-indicators -bool true
+
 echo "Finder: allow text selection in Quick Look"
 defaults write com.apple.finder QLEnableTextSelection -bool true
+
+echo "Finder: show status bar"
+defaults write com.apple.finder ShowStatusBar -bool true
 
 echo "Finder: show icons for hard drives, servers, and removable media on the desktop"
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
