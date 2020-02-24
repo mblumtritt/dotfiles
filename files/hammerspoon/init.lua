8a667022a8643d99b211a7977ba99540006b85ba
Mike Blumtritt <mblumtritt@users.noreply.github.com>
2020-02-24 20:56:58 +0100

    Reduce Hammorspoon foodprint

--- files/hammerspoon/init.lua
+++ files/hammerspoon/init.lua
@@ -1,19 +1,5 @@
+require('watch-cfg')
 require('windowing')
 require('watch-wifi')
-require('watch-cfg')
 -- require('watch-usb')
-
-local clippy = hs.loadSpoon("ClipboardTool")
-clippy:bindHotkeys(
-	{
-		toggle_clipboard = { { "cmd", "control" }, "v" },
-		show_clipboard = { { "cmd", "shift" }, "c" },
-		clear_last_item = { { "cmd", "shift" }, "x" },
-	}
-)
-clippy.show_in_menubar = false
-clippy.hist_size = 25
-clippy.paste_on_select = true
-clippy:start()
-
 hs.dockicon.hide()
