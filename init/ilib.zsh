a914f1c403e0f84e413c24bf40156d8d6c5c9dd3
Mike Blumtritt <mike.blumtritt@pm.me>
2023-08-04 16:17:25 +0200

    Add interactive helper functions

--- init/ilib.zsh
+++ init/ilib.zsh
@@ -23,9 +23,70 @@ lc() # list commands
 	list-commands "$@" | column -x
 }
 
+npass() # add new name/password pair to password store
+{
+	[ $# = 0 ] && { >&2 echo "pass-name missing" && return 1 }
+	[ $# = 1 ] && { >&2 echo "name/id missing" && return 1 }
+	echo "$2" | pass insert -e "$1/name"
+	generate-password | pass insert -e "$1/pass"
+}
+
 @tr() # lookup in GE/EN dictionary
 {
   fetch-web "$(print-search-url --dict "$@")" | convert-html-text | less
+}
+
+browse() # open prefered browser
+{
+	open -a /Applications/Safari.app $@
+}
+
+git_url() # URL of current project
+{
+	echo ${$(git remote get-url origin)/.git/$nop}
+}
+
+git_branch_name() # current branch name
+{
+	basename "$(git symbolic-ref HEAD)"
+}
+
+@gh() # GitHub command
+{
+	case "$1" in
+	-h|--help)
+		cat <<- HELP
+			Options:
+			<name>   open branch <name>
+			-b       open current branch
+			-pr      open pull requests
+			-pc      open pull request for current branch
+			-pn      create pull request for current branch
+			-px      open pull request #x
+		HELP
+		;;
+	-b)
+		browse "$(git_url)/tree/$(git_branch_name)"
+		;;
+	-pr)
+		browse "$(git_url)/pulls"
+		;;
+	-pc)
+		browse "$(git_url)/pull/$(git_branch_name)"
+		;;
+	-pn)
+		browse "$(git_url)/pull/new/$(git_branch_name)"
+		;;
+	-px)
+		browse "$(git_url)/pull/$2"
+		;;
+	'')
+		browse "$(git_url)"
+	;;
+	*)
+		browse "$(git_url)/tree/$1"
+		;;
+	esac
 }
 
 \#() # find and execute command with options
