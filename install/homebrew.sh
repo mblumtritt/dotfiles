53690d7d1473f63dc21dbb4abe0596dbc1edcf7d
Mike Blumtritt <mike.blumtritt@pm.me>
2023-10-31 11:45:22 +0100

    Disable Homebrew analytics by default

--- install/homebrew.sh
+++ install/homebrew.sh
@@ -5,6 +5,7 @@ set -e
 (command -v brew >/dev/null 2>&1) || {
   echo 'install: Homebrew'
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew analytics off
   brew tap homebrew/bundle
   brew bundle --global
 }
