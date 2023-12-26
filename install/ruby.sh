9b57c47eede7efdc5b0dabeeda38094bf7948384
Mike Blumtritt <mike.blumtritt@pm.me>
2023-12-26 17:31:02 +0100

    Update to Ruby v3.3.0

--- install/ruby.sh
+++ install/ruby.sh
@@ -8,6 +8,6 @@ readonly _GEM
 (command -v "$_GEM" >/dev/null 2>&1) && {
   echo 'install: Ruby basic gems'
   $_GEM update --system --no-document
+  $_GEM install --no-document bundler rake pry rubocop syntax_tree tcp-client host-os natty-ui
   $_GEM update --no-document
-  $_GEM install bundler rake pry rubocop syntax_tree tcp-client host-os natty-ui
 }
