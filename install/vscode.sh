ab789ee9c1e2d79be8ee824e0e1b741d60ac0b5c
Mike Blumtritt <mike.blumtritt@pm.me>
2024-04-18 21:20:18 +0200

    Update VSCode config

--- install/vscode.sh
+++ install/vscode.sh
@@ -8,30 +8,28 @@
   code --install-extension --force  andys8.jest-snippets
   code --install-extension --force  ban.spellright
   code --install-extension --force  bmalehorn.shell-syntax
+  code --install-extension --force  britesnow.vscode-toggle-quotes
   code --install-extension --force  christian-kohler.npm-intellisense
   code --install-extension --force  christian-kohler.path-intellisense
   code --install-extension --force  christianoetterli.refactorix
   code --install-extension --force  dbaeumer.vscode-eslint
   code --install-extension --force  esbenp.prettier-vscode
   code --install-extension --force  formulahendry.code-runner
-  code --install-extension --force  GitHub.copilot
-  code --install-extension --force  GitHub.copilot-chat
   code --install-extension --force  hbenl.vscode-test-explorer
   code --install-extension --force  infeng.vscode-react-typescript
   code --install-extension --force  jasonnutter.search-node-modules
   code --install-extension --force  kavod-io.vscode-jest-test-adapter
+  code --install-extension --force  miguel-savignano.ruby-symbols
   code --install-extension --force  misogi.ruby-rubocop
   code --install-extension --force  ms-vscode.cmake-tools
   code --install-extension --force  ms-vscode.cpptools
   code --install-extension --force  ms-vscode.cpptools-extension-pack
   code --install-extension --force  ms-vscode.cpptools-themes
   code --install-extension --force  ms-vscode.test-adapter-converter
-  code --install-extension --force  Orta.vscode-jest
-  code --install-extension --force  rebornix.ruby
+  code --install-extension --force  orta.vscode-jest
   code --install-extension --force  ruby-syntax-tree.vscode-syntax-tree
   code --install-extension --force  scrapecrow.html-escape
-  code --install-extension --force  Thadeu.vscode-run-rspec-file
+  code --install-extension --force  thadeu.vscode-run-rspec-file
   code --install-extension --force  timonwong.shellcheck
-  code --install-extension --force  undefined_publisher.mike
-  code --install-extension --force  wingrunr21.vscode-ruby
+  code --install-extension --force  theaflowers.witch-hazel
 }
