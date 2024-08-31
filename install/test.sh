7c22b818b7ecf48c4a0837118008fc3279c711e9
Mike Blumtritt <mike.blumtritt@pm.me>
2024-08-31 15:00:38 +0200

    Prettify

--- install/test.sh
+++ install/test.sh
@@ -5,7 +5,7 @@ echo 'install: test if required tools are avail'
 for cmd in basename readlink uname bash curl grep
 do
   (>/dev/null 2>&1 command -v "$cmd") || {
-    echo "install: command '$cmd' not found" >&2
+    >&2 echo "install: command '$cmd' not found"
     exit 1
   }
 done
