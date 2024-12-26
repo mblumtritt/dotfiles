083335a40ac47c2b2b27c5a4ea3db9cd0a852cbd
Mike Blumtritt <mike.blumtritt@pm.me>
2024-12-26 22:31:56 +0100

    Update Ruby

--- install/ruby.sh
+++ install/ruby.sh
@@ -5,7 +5,7 @@ readonly _GEM
 
 (command -v "$_GEM" >/dev/null 2>&1) && {
   echo 'install: Ruby basic gems'
-  $_GEM update --system --no-document
+  $_GEM update --no-document --system
   $_GEM install --no-document bundler rake pry rubocop syntax_tree tcp-client host-os natty-ui curb
   $_GEM update --no-document
 }
